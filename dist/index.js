b28689a38d19891c0db8ec1ed75f309cf8eaf75c
Kirill Butyrin <butyrinbusiness@gmail.com>
2024-12-10 18:19:56 +0000

    generate lines fix

--- dist/index.js
+++ dist/index.js
@@ -1,37 +1,40 @@
 "use strict";
-console.log("worked!");
-// Define the SVG namespace
-const SVG_NS = "http://www.w3.org/2000/svg";
 const plane = document.querySelector(".plane");
+const SVG_NS = "http://www.w3.org/2000/svg";
 if (!plane) {
     throw Error("No plane found!");
 }
 const squareSize = plane.clientWidth;
-console.log(squareSize);
 // square sides:
 // left x = 0: coordinates - 0, y, where y in [0, 400]
 // right x = 400: coordinates 400, y, where y in [0, 400]
 // top y = 400: coordinates x, 400, where x in [0, 400]
 // bottom y = 0: coordinates x, 0, where x in [0, 400]
 const squareSides = [
-    { x: 0, y: squareSize }, { x: squareSize, y: squareSize },
-    { x: squareSize, y: squareSize }, { x: squareSize, y: 0 }
+    { x1: 0, y1: () => Math.random() * squareSize },
+    { x1: squareSize, y1: () => Math.random() * squareSize },
+    { x1: () => Math.random() * squareSize, y1: 0 },
+    { x1: () => Math.random() * squareSize, y1: squareSize }
 ];
+const getCoordinateValue = (coordinate) => {
+    return typeof coordinate === "function" ? coordinate().toString() : coordinate.toString();
+};
 const generateLines = (nLines) => {
     const lines = [];
     for (let i = 0; i < nLines; i++) {
+        // Select two random sides of the square
         const startSide = squareSides[Math.floor(Math.random() * squareSides.length)];
         const otherSides = squareSides.filter(side => side !== startSide);
-        const endSide = otherSides[Math.floor(Math.random() * otherSides.length)];
+        const secondSide = otherSides[Math.floor(Math.random() * (otherSides.length))];
+        // Generate line coordinates from the selected sides
         const lineCoordinates = {
-            x1: Math.floor(Math.random() * startSide.x).toString(),
-            y1: Math.floor(Math.random() * startSide.y).toString(),
-            x2: Math.floor(Math.random() * endSide.x).toString(),
-            y2: Math.floor(Math.random() * endSide.y).toString(),
+            x1: getCoordinateValue(startSide.x1),
+            y1: getCoordinateValue(startSide.y1),
+            x2: getCoordinateValue(secondSide.x1),
+            y2: getCoordinateValue(secondSide.y1),
         };
-        console.log(lineCoordinates);
         const line = document.createElementNS(SVG_NS, "line");
-        // Set line attributes through OBJ entries
+        // Set line attributes
         Object.entries(lineCoordinates).forEach(([key, value]) => {
             line.setAttribute(key, value);
         });
@@ -40,10 +43,8 @@ const generateLines = (nLines) => {
     }
     return lines;
 };
-const lines = generateLines(5);
-for (let i = 0; i < lines.length; i++) {
-    const line = lines[i];
-    plane.append(line);
-}
+// Generate and append lines
+const lines = generateLines(6);
+lines.forEach(line => plane.append(line));
 const cuttingAlgorithm = () => {
 };
